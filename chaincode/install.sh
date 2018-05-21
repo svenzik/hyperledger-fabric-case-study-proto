6e389536ebbbf2ede3ab44e6cbb8aea1d77c8165
SM <svenzik@gmail.com>
2018-05-21 12:17:07 -0400

    testing clean installation

---
+++ chaincode/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+go get github.com/golang/protobuf
+go get github.com/digitorus/pkcs7
+go get github.com/digitorus/timestamp
+go get github.com/pierrre/geohash
+go get github.com/shopspring/decimal
+
+# go get github.com/hyperledger/fabric
+# go get github.com/svenzik/hyperledger-fabric-case-study-proto
+
+cp -r $HOME/go/src/github.com/golang .
+cp -r $HOME/go/src/github.com/digitorus .
+cp -r $HOME/go/src/github.com/pierrre .
+cp -r $HOME/go/src/github.com/shopspring .
---
+++ parking-app/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+npm install
+node registerAdmin.js
+node registerUser.js  
+# npm run start
+npm run start-dev
