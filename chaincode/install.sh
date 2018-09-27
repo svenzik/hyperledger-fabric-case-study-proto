f53ff93dc3a57bb7b8c02b2a8bbf25ed5ce19a09
SM <svenzik@gmail.com>
2018-09-27 08:49:50 +0300

    Change allows a different GOPATH & fixed execution permissions

--- chaincode/install.sh
+++ chaincode/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+if [ -z "$GOPATH" ]; then
+	export GOPATH=$HOME/go
+fi
 
 go get github.com/golang/protobuf/protoc-gen-go
 go get github.com/digitorus/pkcs7
@@ -9,7 +12,7 @@ go get github.com/shopspring/decimal
 # go get github.com/hyperledger/fabric
 # go get github.com/svenzik/hyperledger-fabric-case-study-proto
 
-cp -r $HOME/go/src/github.com/golang .
-cp -r $HOME/go/src/github.com/digitorus .
-cp -r $HOME/go/src/github.com/pierrre .
-cp -r $HOME/go/src/github.com/shopspring .
+cp -r $GOPATH/src/github.com/golang .
+cp -r $GOPATH/src/github.com/digitorus .
+cp -r $GOPATH/src/github.com/pierrre .
+cp -r $GOPATH/src/github.com/shopspring .
